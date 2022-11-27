448a0873753331cf7b5e239f6d8d7f68394cd205
Ricky Hu <ricky9667@gmail.com>
2022-11-27 17:49:57 +0800

    fix: avoid page overflow screen width

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 </script>
 
 <template>
-  <header h-12 md:h-20 lg:h-28 w-screen flex flex-row justify-center items-center gap-2 lg:gap-4 class="header">
+  <header h-12 md:h-20 lg:h-28 flex flex-row justify-center items-center gap-2 lg:gap-4 mx-0 class="header">
     <img w-8 md:w-12 lg:w-16 src="./assets/logo.png">
     <h1 text-white text-xl md:text-3xl font-bold>
       超級智慧潛水
     </h1>
   </header>
+
   <main font-sans h-full p="x-4 y-4" text="center gray-700">
     <router-view />
   </main>
