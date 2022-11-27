98e305f7566bfaa311bf60f456449af313e0759b
Xanonymous <trusaidlin@gmail.com>
2022-11-28 01:51:50 +0800

    security: fix random seed crypto

--- src/data/oceanDepthChart.ts
+++ src/data/oceanDepthChart.ts
@@ -5,12 +5,13 @@ let base = +new Date(1968, 9, 3)
 const oneDay = 24 * 3600 * 1000
 const date = []
 
-const data = [Math.random() * 300]
+const data = [useRandom(1, 300)]
 
 for (let i = 1; i < 20000; i++) {
   const now = new Date((base += oneDay))
   date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'))
-  data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]))
+  const randomValue = useRandom(-10, 10)
+  data.push(Math.round((randomValue + data[i - 1])))
 }
 
 export const oceanDepthChartOption: EChartsOption = {
