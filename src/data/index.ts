3934423bd807e11f6fa2870e1a59073fc5977cd2
Xanonymous <trusaidlin@gmail.com>
2022-12-01 06:11:37 +0800

    feat: use sized queue to restrict max amount of chart data

--- src/data/index.ts
+++ src/data/index.ts
@@ -15,18 +15,21 @@ import { getTemperatureChartOption } from '~/data/temperatureChart'
 import { getUltraVioletChartOption } from '~/data/uvChart'
 import { getWindChartOption } from '~/data/windChart'
 
+const MAX_STORED_CHART_DATA_AMOUNT = 20
+const DEBOUNCE_TIME = 300
+
 // firebase data.
-const flightPathPointData = $ref<Array<Readonly<NonNullable<FlightPathPointData>>>>([])
-const ultraVioletData = $ref<Array<Readonly<NonNullable<UltraVioletData>>>>([])
-const temperatureData = $ref<Array<Readonly<NonNullable<TemperatureData>>>>([])
-const waterDepthData = $ref<Array<Readonly<NonNullable<WaterDepthData>>>>([])
-const windData = $ref<Array<Readonly<NonNullable<WindData>>>>([])
+const flightPathPointData = shallowRef(useQueueWithSize<FlightPathPointData>(MAX_STORED_CHART_DATA_AMOUNT))
+const ultraVioletData = shallowRef(useQueueWithSize<UltraVioletData>(MAX_STORED_CHART_DATA_AMOUNT))
+const temperatureData = shallowRef(useQueueWithSize<TemperatureData>(MAX_STORED_CHART_DATA_AMOUNT))
+const waterDepthData = shallowRef(useQueueWithSize<WaterDepthData>(MAX_STORED_CHART_DATA_AMOUNT))
+const windData = shallowRef(useQueueWithSize<WindData>(MAX_STORED_CHART_DATA_AMOUNT))
 
 // chart options
-const ultraVioletChartOption = $computed(() => getUltraVioletChartOption(ultraVioletData))
-const temperatureChartOption = $computed(() => getTemperatureChartOption(temperatureData))
-const oceanDepthChartOption = $computed(() => getOceanDepthChartOption(waterDepthData))
-const windChartOption = $computed(() => getWindChartOption(windData))
+const ultraVioletChartOption = $computed(() => getUltraVioletChartOption(ultraVioletData.value.toArray()))
+const temperatureChartOption = $computed(() => getTemperatureChartOption(temperatureData.value.toArray()))
+const oceanDepthChartOption = $computed(() => getOceanDepthChartOption(waterDepthData.value.toArray()))
+const windChartOption = $computed(() => getWindChartOption(windData.value.toArray()))
 
 const updateLiveStreamedData = (data?: Readonly<ChartData>) => {
   if (!data)
@@ -40,11 +43,30 @@ const updateLiveStreamedData = (data?: Readonly<ChartData>) => {
     wind,
   } = data
 
-  coordinate && flightPathPointData.push(coordinate)
-  ultraviolet && ultraVioletData.push(ultraviolet)
-  temperature && temperatureData.push(temperature)
-  water_depth && waterDepthData.push(water_depth)
-  wind && windData.push(wind)
+  coordinate && (() => {
+    flightPathPointData.value.enqueue(coordinate)
+    triggerRef(flightPathPointData)
+  })()
+
+  ultraviolet && (() => {
+    ultraVioletData.value.enqueue(ultraviolet)
+    triggerRef(ultraVioletData)
+  })()
+
+  temperature && (() => {
+    temperatureData.value.enqueue(temperature)
+    triggerRef(temperatureData)
+  })()
+
+  water_depth && (() => {
+    waterDepthData.value.enqueue(water_depth)
+    triggerRef(waterDepthData)
+  })()
+
+  wind && (() => {
+    windData.value.enqueue(wind)
+    triggerRef(windData)
+  })()
 }
 
 export const liveChartDataListener: Plugin = {
@@ -55,9 +77,9 @@ export const liveChartDataListener: Plugin = {
       const database = getDatabase(firebaseApp)
       const dataRef = firebaseRef(database, 'datas/')
 
-      return onValue(dataRef, (snapshot) => {
+      return onValue(dataRef, useDebounceFn((snapshot) => {
         updateLiveStreamedData(snapshot.val() as unknown as ChartData)
-      })
+      }, DEBOUNCE_TIME))
     }
 
     tryOnBeforeMount(() => {
