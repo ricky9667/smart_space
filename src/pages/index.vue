9cac8a9053a3940d99ecfe206acccb249bb0a951
Ricky Hu <ricky9667@gmail.com>
2022-11-30 20:51:15 +0800

    feat: create function to add snapshot data to arrays

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import type { EChartsOption } from 'echarts'
 import { ref as firebaseRef, getDatabase, onValue } from 'firebase/database'
-import { flightPlanHeight } from '~/config/config'
 import { firebaseApp } from '~/config/firebase'
 import options from '~/data'
+import type { FlightPathPointData, TemperatureData, UltraVioletData, WaterDepthData, WindData } from '~/data/chartDataTypes'
 
 const chartOptions = $shallowRef<Array<EChartsOption>>(options)
 
@@ -14,59 +14,27 @@ const chartIds = [
   'wind-chart',
 ]
 
-var flightPathPointDatas = [];
-var airDatas = [];
-var waterDatas = [];
-var ultravioletDatas = [];
-var waterDepthDatas = [];
-var windDatas = [];
+const flightPathPointDatas: Array<FlightPathPointData> = []
+const ultraVioletDatas: Array<UltraVioletData> = []
+const temperatureDatas: Array<TemperatureData> = []
+const waterDepthDatas: Array<WaterDepthData> = []
+const windDatas: Array<WindData> = []
+
+const addCurrentSnapshot = (data: any) => {
+  flightPathPointDatas.push({ latitude: data.coordinate.latitude, longitude: data.coordinate.longitude })
+  ultraVioletDatas.push({ amount: data.ultraviolet.amount, time: data.ultraviolet.time })
+  temperatureDatas.push({ air: data.temperature.air, water: data.temperature.water, time: data.temperature.time })
+  waterDepthDatas.push({ depth: data.water_depth.value, time: data.water_depth.time })
+  windDatas.push({ direction: data.wind.direction, speed: data.wind.speed })
+}
 
 const listenFirebaseData = () => {
   const database = getDatabase(firebaseApp)
   const dataRef = firebaseRef(database, 'datas/')
 
   onValue(dataRef, (snapshot) => {
-    showData(snapshot.val());
+    addCurrentSnapshot(snapshot.val())
   })
-}
-
-const showData = (data: any) => {
-  var latitude = data["coordinate"]["latitude"]
-  var longitude = data["coordinate"]["longitude"]
-  flightPathPointDatas.push(createFlightPointsData(latitude, longitude))
-  var air = data["temperature"]["air"]
-  var time = data["temperature"]["time"]
-  var water = data["temperature"]["water"]
-  airDatas.push(create2DData(air, time))
-  waterDatas.push(create2DData(water, time))
-  var amount = data["ultraviolet"]["amount"]
-  // var time = data["ultraviolet"]["time"]
-  ultravioletDatas.push(create2DData(amount, time))
-  // var time = data["water_depth"]["time"]
-  var value = data["water_depth"]["value"]
-  waterDepthDatas.push(create2DData(value, time))
-  var direction = data["wind"]["direction"]
-  var speed = data["wind"]["speed"]
-  windDatas.push(create2DData(direction, speed))
-}
-
-const createFlightPointsData = (latitude, longitude, value = 0) => {
-  var coordinateData = {
-      name: '',
-      value: [longitude, latitude, flightPlanHeight, value],
-      itemStyle: { 'color': 'white', 'opacity': 0.8, 'borderWidth': 3, 'borderColor': 'green' }
-    }
-  return coordinateData;
-}
-
-const create2DData = (value, data_time) => {
-  return {
-    name: data_time,
-    value: [
-      data_time,
-      value
-    ]
-  }
 }
 
 tryOnMounted(() => {
